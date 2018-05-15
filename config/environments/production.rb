e5825a9cf92ab17dcf06d1799d5f40b6de86e539
Jahan Khan <jahanatakhan@gmail.com>
2018-05-15 17:18:48 -0400

    heroku3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  ENV["DISABLE_DATABASE_ENVIRONMENT_CHECK"] = "1"
 end
