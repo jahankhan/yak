5de9761c24fedc663c93eed0a2bdb2c030b2ef0c
Jahan Khan <jahanatakhan@gmail.com>
2018-05-16 17:07:35 -0400

    heroku fix hopefully for avatar change

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,11 @@ module Yak
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       :storage => :s3,
+      :bucket => ENV["s3_bucket"],
+      :s3_region => ENV["s3_region"],
       :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => "us-east-2",
         :s3_host_name => "s3.#{ENV["s3_region"]}.amazonaws.com"
       }
     }
